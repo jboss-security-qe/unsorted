7720f10ca9625e9928e2df5fc0e39bd8962e4f07
Josef Cacek <josef.cacek@gmail.com>
2017-03-09 14:16:32 +0100

    Fixed param (and added more)

--- scripts/prepareLadybird.sh
+++ scripts/prepareLadybird.sh
@@ -35,7 +35,7 @@ VERSION_ELY=$(fetchAndBuild wildfly-elytron git@github.com:wildfly-security/wild
 VERSION_ELYWEB=$(fetchAndBuild elytron-web git@github.com:wildfly-security/elytron-web.git master -Dversion.org.wildfly.security.elytron=$VERSION_ELY)
 VERSION_ELYTOOL=$(fetchAndBuild wildfly-elytron-tool git@github.com:wildfly-security/wildfly-elytron-tool.git master -Dversion..elytron=$VERSION_ELY)
 VERSION_WFCORE=$(fetchAndBuild wildfly-core git@github.com:wildfly-security-incubator/wildfly-core.git ladybird -Dversion.org.wildfly.security.elytron=$VERSION_ELY -Dversion.org.wildfly.security.elytron.tool=$VERSION_ELYTOOL -Dversion.org.wildfly.security.elytron-web.undertow-server=$VERSION_ELYWEB)
-VERSION_WFLY=$(fetchAndBuild wildfly git@github.com:wildfly-security-incubator/wildfly.git ladybird Dversion.org.wildfly.core=$VERSION_WFCORE)
+VERSION_WFLY=$(fetchAndBuild wildfly git@github.com:wildfly-security-incubator/wildfly.git ladybird -Dversion.org.wildfly.core=$VERSION_WFCORE -Dversion.org.wildfly.security.elytron=$VERSION_ELY -Dversion.org.wildfly.security.elytron-web.undertow-server=$VERSION_ELYWEB)
 
 echo "To rebuild the wildfly (ladybird) use:"
 echo "  mvn clean install -DskipTests -Dcheckstyle.skip -Denforcer.skip -Dversion.org.wildfly.core=$VERSION_WFCORE"
